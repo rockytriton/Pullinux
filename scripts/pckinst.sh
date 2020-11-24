5553724ad8abd1dfca48fdb6317cdceaf158cfed
rockytriton <rockytriton@yahoo.com>
2020-11-23 18:43:00 -0600

    update scripts

--- scripts/pckinst.sh
+++ scripts/pckinst.sh
@@ -1,11 +1,28 @@
-pck=$1
+fullpck=$1
+pck=$(basename $1)
 p2=$2
 dst=${p2:-/}
 
 echo "Installing ${pck:?} to ${dst:?}..."
 
+if [ -d "/tmp/_plxinst" ]; then
+	rm -rf /tmp/_plxinst
+fi
+
+mkdir /tmp/_plxinst
+cp $fullpck /tmp/_plxinst
+
+tar -xf /tmp/_plxinst/$pck -C /tmp/_plxinst ./.dep 2> /dev/null
+
+if [ -f "/tmp/_plxinst/.dep" ]; then
+	echo "Has Dependencies:"
+	cat /tmp/_plxinst/.dep
+else
+	echo "No Dependencies"
+fi
+
 cd $dst
-tar -hxf $pck
+tar -hxf $fullpck
 
 if [ -f "_install/preinstall.sh" ]; then
 	cd _install
